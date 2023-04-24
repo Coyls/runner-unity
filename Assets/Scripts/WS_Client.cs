c1934dd13834c6a2eac6a4b919d26037b366c5b0
Coyls <loic.grecofr@gmail.com>
2023-04-24 19:33:11 +0200

    nada

--- Assets/Scripts/WS_Client.cs
+++ Assets/Scripts/WS_Client.cs
@@ -12,11 +12,16 @@ public class WS_Client : MonoBehaviour
     [SerializeField] float sidewaysForce = 300f;
     void Start()
     {
-        ws = new WebSocket("ws://192.168.1.97:3000");
+        // ws = new WebSocket("ws://192.168.1.97:3000");
+        ws = new WebSocket("ws://localhost:3000");
 
         ws.OnMessage += OnMessage;
 
         ws.Connect();
+
+        /* yield return StartCoroutine(ws.Connect());
+
+        yield return 0; */
     }
 
     // Update is called once per frame
@@ -35,20 +40,35 @@ public class WS_Client : MonoBehaviour
 
     void OnMessage(object sender, MessageEventArgs e)
     {
-        StartCoroutine(ApplyForce(e.Data));
+        // StartCoroutine(ApplyForce(e.Data));
+
+        Debug.Log("PLSSSSS");
+
+
+        if (e.Data == "RIGHT")
+        {
+            Debug.Log("Right");
+            rb.AddForce(new Vector3(sidewaysForce * Time.deltaTime, 0, 0), ForceMode.VelocityChange);
+        }
+        if (e.Data == "LEFT")
+        {
+            Debug.Log("Left");
+            rb.AddForce(new Vector3(-sidewaysForce * Time.deltaTime, 0, 0), ForceMode.VelocityChange);
+        }
     }
 
-    IEnumerator ApplyForce(string direction)
+    /* IEnumerator ApplyForce(string direction)
     {
         yield return new WaitForFixedUpdate();
 
-        if (direction == "RIGHT")
+        
+    } */
+
+    private void OnDestroy()
+    {
+        if (ws != null && ws.IsAlive)
         {
-            rb.AddForce(new Vector3(sidewaysForce * Time.deltaTime, 0, 0), ForceMode.VelocityChange);
-        }
-        if (direction == "LEFT")
-        {
-            rb.AddForce(new Vector3(-sidewaysForce * Time.deltaTime, 0, 0), ForceMode.VelocityChange);
+            ws.Close();
         }
     }
 
